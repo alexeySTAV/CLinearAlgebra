179b67b9556533b43f0f1518034a1a0f190ef202
alexeySTAV <wehheh32@gmail.com>
2023-11-29 13:11:40 +0600

    Added determinant, created matrixFuncs.h, added it to wrapper

--- CLinearAlgebra.h
+++ CLinearAlgebra.h
@@ -1,3 +1,4 @@
 #pragma once
 #include "elements/matrix.h"
 #include "elements/vector.h"
+#include "funcs/matrixFuncs.h"
--- elements/matrix.h
+++ elements/matrix.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <initializer_list>
 #include <stdexcept>
+#include <cmath>
 
 namespace linal
 {
@@ -41,6 +42,26 @@ namespace linal
             return rows_;
         }
         
+        matrix transpose()
+        {
+            matrix<T> res(columns_, rows_);
+            for(int i = 0; i < rows_; i++)
+                for(int j = 0; j < columns_; j++)
+                    res[j][i] = (*this)[i][j];
+            return res;
+        }
+
+        T det()
+        {
+            T res = T();
+            if(columns_ != rows_)
+                throw std::invalid_argument("On this quiz, I asked you to find the determinant of a 2x3 matrix.\nSome of you, to my great amusement, actually tried to do this.\n(Via mathprofessorquotes.com, no name listed)");
+            if(columns_ == 2 && rows_ == 2)
+                return (*this)[0][0] * (*this)[1][1] - (*this)[1][0] * (*this)[0][1];
+            for(int i = 0; i < columns_; i++)
+                res += (*this)[0][i] * pow(-1, i) * cutMatrix(*this, 0, i).det();
+            return res;
+        }
         
         private:
         class Row 
@@ -84,6 +105,21 @@ namespace linal
             if (rowIndex >= rows_) 
                 throw std::out_of_range("Rows out of range!");
             return Row(&data_[rowIndex * columns_], columns_);
+        }
+        friend std::ostream& operator<<(std::ostream& os, const matrix<T>& mat) {
+            os << std::string("{\n");
+            for (size_t i = 0; i < mat.rows(); ++i) {
+                os << std::string("  {");
+                for (size_t j = 0; j < mat.columns(); ++j) {
+                    os << mat[i][j];
+                    if (j < mat.columns() - 1) os << std::string(", ");
+                }
+                os << std::string("}");
+                if (i < mat.rows() - 1) os << std::string(",");
+                os << std::string("\n");
+            }
+            os << std::string("}");
+            return os;
         }
         inline matrix& operator=(const matrix& other)
         {
@@ -174,10 +210,31 @@ namespace linal
                     (*this)[i][j] /= scalar;
             return *this;
         }
-        
+
         private:
         size_t rows_, columns_;
         T* data_ = new T[rows_ * columns_];
+        
+        matrix cutMatrix(matrix& toCut, size_t Index, size_t Jndex)
+        {
+            matrix<T> res(toCut.rows() - 1, toCut.columns() - 1);
+            int itr = 0, jtr = 0;
+            for(int i = 0; i < toCut.rows(); i++)
+            {
+                jtr = 0;
+                if(i == Index)
+                    continue;
+                for(int j = 0; j < toCut.columns(); j++)
+                {
+                    if(j == Jndex)
+                        continue;
+                    res[itr][jtr] = toCut[i][j];
+                    jtr++;
+                }
+                itr++;
+            }
+            return res;
+        }
 
     };
 }
